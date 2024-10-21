1b375eb155ea78409b4d4c1e478c4a337dde8334
Beni Samuel <benisamuel566@gmail.com>
2024-10-21 10:40:10 +0200

    Added Arraylist

--- com/product/model/ProductModel.java
+++ com/product/model/ProductModel.java
@@ -1,14 +1,17 @@
 package model;
+import java.util.ArrayList;
 
 public class ProductModel {
     private String name;
     private String description;
     private int price;
+    static ArrayList<ProductModel> products = new ArrayList<ProductModel>();
 
     public ProductModel (String name, String description, int price){
         this.name = name;
         this.description = description;
         this.price = price;
+        products.add(this);
     }
 
     public String getName(){
